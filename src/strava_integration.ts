8add7dffd3f2cdd193510912fff663803ecd02c0
Seth Herr <seth.william.herr@gmail.com>
2023-06-06 13:11:39 -0700

    add jest

---
+++ src/strava_integration.ts
@@ -0,0 +1,19 @@
+// periods = []
+
+// // https://www.strava.com/clubs/278109
+
+// function getUserList() {
+//   // TODO: get user list from strava mayIsBikeMonth
+// }
+
+// function userTotalsForPeriod(stravaId, period): Totals {
+
+// }
+
+export function add (x: number, y: number): number {
+  return x + y
+}
+
+export function multiply (x: number, y: number): number {
+  return x * y
+}

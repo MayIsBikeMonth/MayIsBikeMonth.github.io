ed4db51f903e25f9302189b4e8576cfe6853d8d0
Seth Herr <seth.william.herr@gmail.com>
2023-06-07 11:27:22 -0700

    making some strava requests

--- src/strava_integration.ts
+++ src/strava_integration.ts
@@ -5,22 +5,58 @@ const stravaToken = process.env.STRAVA_TOKEN
 const stravaClientID = process.env.STRAVA_CLIENT_ID
 
 const periods = []
-const clubId = 278109
+const clubId = 278109 // No longer super relevant
+const countedSportTypes = [
+  // 'EBikeRide',
+  // 'EMountainBikeRide',
+  'GravelRide',
+  'MountainBikeRide',
+  'Ride',
+  'VirtualRide',
+  'Velomobile',
+  'Wheelchair',
+  'Handcycle'
+]
 
-console.log(stravaClientID)
+// Useful things from the strava API docs:
+// resource_state: indicates level of detail. Possible values: 1 -> "meta", 2 -> "summary", 3 -> "detail"
+// moving_time: The activity's moving time, in seconds (if )
+// start_date_local: The activity's start date, in "Local" format
+// distance: The activity's distance, in meters
+// total_elevation_gain: The activity's total elevation gain, in meters
+// sport_type: instance of SportType
 
-// function getUserList() {
-//   // TODO: get user list from strava mayIsBikeMonth
-// }
+export async function makeGetRequest (endpoint: string, args: object = {}): Promise<object> {
+  if (endpoint === 'club') { endpoint = `clubs/${clubId}` }
+
+  const response = await fetch(`https://www.strava.com/api/v3/${endpoint}`, {
+    method: 'GET',
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${stravaToken}`
+    }
+  })
+  // .catch((e) => {
+  //     if (e.response && e.response.headers) {
+  //       limits = rateLimiting.updateRateLimits(e.response.headers)
+  //     }
+
+  //     return Promise.reject(e)
+  //   })
+  const jsonData = await response.json()
+  return jsonData
+}
+
+export async function getUserList () {
+  const endpoint = `clubs/${clubId}/members`
+  const args = { per_page: 200 }
+  const result = await makeGetRequest(endpoint, args)
+  return result
+}
 
 // function userTotalsForPeriod(stravaId, period): Totals {
 
 // }
-
-export function add (x: number, y: number): number {
-  return x + y
-}
-
-export function multiply (x: number, y: number): number {
-  return x * y
-}
--- tests/strava_integration.test.ts
+++ tests/strava_integration.test.ts
@@ -1,13 +1,27 @@
-import { add, multiply } from "../src/strava_integration";
+import { type } from "os";
+import * as strava from "../src/strava_integration";
 
 describe("Math functions", () => {
   it("should multiply 5 by 3", () => {
-    const result = multiply(5, 3);
+    const result = strava.multiply(5, 3);
     expect(result).toEqual(15);
   });
 
   it("should add 5 by 3", () => {
-    const result = add(5, 3);
+    const result = strava.add(5, 3);
     expect(result).toEqual(8);
   });
+});
+
+test("makeGetRequest", async () => {
+  // const result = await strava.makeGetRequest("athlete/activities", {per_page: 200});
+  const result = await strava.makeGetRequest("club");
+  console.log(result)
+  expect(result).toEqual(8);
+});
+
+test("getUserList", async () => {
+  const result = await strava.getUserList();
+  // console.log(result[0])
+  expect(result.length).toEqual(17);
 });
